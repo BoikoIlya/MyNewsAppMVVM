0c8b7c6a210238fcb9654a955f8c1abe280cf980
BoikoIlya <inmortal.ilya@yandex.by>
2022-07-19 20:33:45 +0300

    Setting nav graph with bottom nav

--- app/src/main/java/com/ilya/mynewsapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/ilya/mynewsapp/presentation/activities/MainActivity.kt
@@ -2,11 +2,24 @@ package com.ilya.mynewsapp.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ilya.mynewsapp.R
+import com.ilya.mynewsapp.presentation.fragments.BreakingNewsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        BottomoNavigationMenu.setupWithNavController(navController)
     }
 }
