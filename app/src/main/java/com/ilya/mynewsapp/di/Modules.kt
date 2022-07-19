8318d1974c7870fe7c6d12dc2714e7297662a89e
BoikoIlya <inmortal.ilya@yandex.by>
2022-07-19 18:55:56 +0300

    Adding Modules class

---
+++ app/src/main/java/com/ilya/mynewsapp/di/Modules.kt
@@ -0,0 +1,11 @@
+package com.ilya.mynewsapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class Modules {
+
+}
