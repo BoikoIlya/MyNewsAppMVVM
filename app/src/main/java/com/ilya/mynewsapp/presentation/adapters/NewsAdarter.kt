08f3d54b4dcd05edeacc90b1bca7ba258360e6ca
BoikoIlya <inmortal.ilya@yandex.by>
2022-07-20 22:09:34 +0300

    Adding News Adapter

---
+++ app/src/main/java/com/ilya/mynewsapp/presentation/adapters/NewsAdarter.kt
@@ -0,0 +1,54 @@
+package com.ilya.mynewsapp.presentation.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ilya.mynewsapp.R
+import com.ilya.mynewsapp.databinding.NewsItemBinding
+import com.ilya.mynewsapp.model.Article
+
+class NewsAdarter: RecyclerView.Adapter<NewsAdarter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: NewsItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    private val differCallBack = object: DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+         return oldItem.title==newItem.title
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem==newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this,differCallBack)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+       return ViewHolder(
+           NewsItemBinding.inflate(
+               LayoutInflater.from(parent.context),
+               parent,
+               false
+            )
+       )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.binding.apply {
+            title.text = article.title
+            description.text = article.description
+            source.text = article.source.name
+            publishedAt.text = article.publishedAt
+            Glide.with(holder.itemView).load(article.urlToImage).into(img)
+        }
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+}
