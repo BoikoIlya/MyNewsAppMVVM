91187a455a69dafe752a2bd77cb9e3dc00a3aea8
BoikoIlya <inmortal.ilya@yandex.by>
2022-07-20 19:00:38 +0300

    Adding Repository interface functions

--- app/src/main/java/com/ilya/mynewsapp/data/Repository.kt
+++ app/src/main/java/com/ilya/mynewsapp/data/Repository.kt
@@ -1,4 +1,18 @@
 package com.ilya.mynewsapp.data
 
+import com.ilya.mynewsapp.model.Article
+import com.ilya.mynewsapp.model.NewsResponse
+import retrofit2.Response
+
 interface Repository {
+
+    suspend fun getApi():Response<NewsResponse>
+
+    suspend fun searchApi(name: String):Response<NewsResponse>
+
+    suspend fun saveOrUpdateDataBase(name: String)
+
+    suspend fun readFromDataBase():List<Article>
+
+    suspend fun deleteFromDataBase(id: Int)
 }
