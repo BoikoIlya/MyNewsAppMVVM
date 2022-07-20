562c0d3181e9f6cf6dff928e93c9c2efb51bae2c
BoikoIlya <inmortal.ilya@yandex.by>
2022-07-20 19:13:22 +0300

    Adding Retrofit instance

--- app/src/main/java/com/ilya/mynewsapp/data/Repository.kt
+++ app/src/main/java/com/ilya/mynewsapp/data/Repository.kt
@@ -2,13 +2,14 @@ package com.ilya.mynewsapp.data
 
 import com.ilya.mynewsapp.model.Article
 import com.ilya.mynewsapp.model.NewsResponse
+import com.ilya.mynewsapp.utils.Constance
 import retrofit2.Response
 
 interface Repository {
 
-    suspend fun getApi():Response<NewsResponse>
+    suspend fun getApi(apiKey:String):Response<NewsResponse>
 
-    suspend fun searchApi(title: String):Response<NewsResponse>
+    suspend fun searchApi(apiKey:String, title: String):Response<NewsResponse>
 
     suspend fun saveOrUpdateDataBase(name: String)
 
